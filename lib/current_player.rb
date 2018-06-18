9f1491a17441e22d9e12b47a7f18b51f180c744b
Jordan Harris <jharris@wework.com>
2018-06-18 16:26:33 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,46 +2,42 @@ def turn_count(board)
   turns == 0 
   while turns >= 9
   board.each do |user|
-    turns += 1
     if user =! "" || user =! " "
+      turns += 1
   end
-
-  
   return turn_count
 end
 
 
-def current_player
+# def current_player
   
-end
+# end
 
-  
-  
   
   
   
-   it 'counts occupied positions' do
-      board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+  # it 'counts occupied positions' do
+  #     board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
 
-      expect(turn_count(board)).to eq(3)
-    end
-  end
+  #     expect(turn_count(board)).to eq(3)
+  #   end
+  # end
 
-  describe '#current_player' do
-    it 'returns the correct player, X, for the first move' do
-      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  # describe '#current_player' do
+  #   it 'returns the correct player, X, for the first move' do
+  #     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-      expect(current_player(board)).to eq("X")
-    end
+  #     expect(current_player(board)).to eq("X")
+  #   end
 
-    it 'returns the correct player, O, for the second move' do
-      board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+  #   it 'returns the correct player, O, for the second move' do
+  #     board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 
-      expect(current_player(board)).to eq("O")
-    end
+  #     expect(current_player(board)).to eq("O")
+  #   end
 
-    it 'returns the correct player, X, for the third move' do
-      board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+  #   it 'returns the correct player, X, for the third move' do
+  #     board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
 
-      expect(current_player(board)).to eq("X")
-    end
+  #     expect(current_player(board)).to eq("X")
+  #   end
